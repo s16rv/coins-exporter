886b2ade32be3fdc8c955c4b030276cca43075fe
Miftahul Arifin <miftahul97@gmail.com>
2022-12-06 10:35:45 +0700

    fix: golang ci

--- query.go
+++ query.go
@@ -16,15 +16,20 @@ func SendQueryCoinsDetail(baseApi, id string) (types.ReturnData, error) {
 
 	req, _ := http.NewRequest("GET", u, nil)
 	resp, err := client.Do(req)
+	if err != nil {
+		return d, err
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return d, err
 	}
+
 	err = json.Unmarshal(body, &d)
 	if err != nil {
 		return d, err
 	}
+
 	defer resp.Body.Close()
 
 	return d, nil
